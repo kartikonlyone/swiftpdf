1374c886692b8868a62698f0722e2a4b3be93c63
Kartik Sharma <kartiknd999@gmail.com>
2026-09-17 20:00:05 +0530

    Wrap NextAuth handler to prevent build pre-rendering crash

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,5 +5,7 @@ export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs'; 
 export const revalidate = 0;
 
-const handler = NextAuth(authOptions);
+const handler = async (req, res) => {
+  return await NextAuth(req, res, authOptions);
+};
 export { handler as GET, handler as POST };
